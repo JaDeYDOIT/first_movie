d86107b241857496eecd70ac7e2f131b333df76e
AoClear <116623850+AoClear@users.noreply.github.com>
2024-01-11 10:03:55 +0900

    commit

--- src/main/java/kr/co/fmos/movie/MovieDAO.java
+++ src/main/java/kr/co/fmos/movie/MovieDAO.java
@@ -13,46 +13,5 @@ public interface MovieDAO {
 
 	public int reviewInsert(MoviereviewDTO dto);
 
-	// 영화 리스트
-	public List<MovieDTO> movieList() {
-		return sqlSession.selectList("movie.list");
-	}// movieList() end
-
-	// 영화 추가
-	public int movieinsert(MovieDTO movieDto) {
-		return sqlSession.insert("movie.insert", movieDto);
-	}// commentInsert() end
-
-	// 영화 상세
-	public MovieDTO movieDetail(String movie_id) {
-		return sqlSession.selectOne("movie.detail", movie_id);
-	}// movieList() end
-
-	// 댓글 리스트
-	public List<MoviereviewDTO> moviereviewList(String movie_id) {
-		return sqlSession.selectList("movie.review", movie_id);
-	}// movieList() end
-	
-	
-	public int reviewInsert(MoviereviewDTO dto) {
-		return sqlSession.insert("movie.reviewInsert", dto);
-	}//insert() end
-	
-//	 맵방식 댓글
-//	 public Map<String, Object> moviereviewList(String movie_id) {
-//			return sqlSession.selectOne("movie.review", movie_id);
-//		}// movieList() end
-
-//	// 사용가능하지만 하나의 댓글만 나옴
-//	 public MoviereviewDTO moviereviewList(String movie_id) {
-//		 return sqlSession.selectOne("movie.review", movie_id);
-//	 }// movieList() end
-	
-// TheaterCont에서 호출하는 메서드 시작
-	public MovieDTO moviecheck(int movie_id) {
-		return sqlSession.selectOne("movie.moviecheck", movie_id);
-	}// movieList() end
-// TheaterCont에서 호출하는 메서드 끝
-
 	public MovieDTO selectMovieInfoById(String movieID);
-}// class end
+}
