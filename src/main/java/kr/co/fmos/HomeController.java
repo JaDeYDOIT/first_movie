a2cae2a9c53639037facd53d354f8c8555d2a94b
AoClear <116623850+AoClear@users.noreply.github.com>
2024-01-17 16:59:19 +0900

    commit

--- src/main/java/kr/co/fmos/HomeController.java
+++ src/main/java/kr/co/fmos/HomeController.java
@@ -1,34 +1,44 @@
 package kr.co.fmos;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpSession;
 
+//first movie 프로젝트의 첫페이지 호출
+//-> http://localhost:9095/main
 @Controller
+@RequestMapping("/main")
 public class HomeController {
-    
+
 	public HomeController() {
-        System.out.println("-----HomeController()객체 생성됨");
-    }
-    
-    //first movie 프로젝트의 첫페이지 호출
-    //-> http://localhost:9095/main    
-	@RequestMapping("/main")
-	public ModelAndView home(HttpSession session ) {
-		if(session.getAttribute("s_id") == null || session.getAttribute("s_pw") == null ||
-		session.getAttribute("s_id").equals("") || session.getAttribute("s_pw").equals("")) 
-		{
+		System.out.println("-----HomeController()객체 생성됨");
+	}
+
+	@RequestMapping
+	public ModelAndView home(HttpSession session) {
+		if (session.getAttribute("s_id") == null || session.getAttribute("s_pw") == null
+				|| session.getAttribute("s_id").equals("") || session.getAttribute("s_pw").equals("")) {
 			String s_id = "guest";
 			String s_pw = "";
 			session.setAttribute("s_id", s_id);
 		}
-		
-     ModelAndView mav = new ModelAndView();
-     mav.addObject("sessionTimeoutInSeconds", session.getMaxInactiveInterval());
-     mav.setViewName("main");
-     return mav;
-	}//home() end
-	
-}//class end
+
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("sessionTimeoutInSeconds", session.getMaxInactiveInterval());
+		mav.setViewName("main");
+		return mav;
+	}// home() end
+
+	// 로그인 여부 판단
+	@PostMapping("/isLoggedIn")
+	@ResponseBody
+	public boolean isLoggedIn(HttpSession session) {
+		String userId = (String) session.getAttribute("s_id");
+
+		return userId != "guest";
+	}
+}// class end
