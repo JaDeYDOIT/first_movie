8d477910b140485cc1f82d90df08bf7775f696e9
AoClear <116623850+AoClear@users.noreply.github.com>
2024-01-18 18:08:59 +0900

    20240118 18:08

--- src/main/java/kr/co/fmos/customer/InquireDAO.java
+++ src/main/java/kr/co/fmos/customer/InquireDAO.java
@@ -29,12 +29,20 @@ public class InquireDAO {
 		return sqlSession.selectList("customer.lostselect", inquire_kind);
 	}// movieList() end
 	
+	public int lostInquireins(InquireDTO dto) {
+		return sqlSession.insert("customer.lostInquireins", dto);
+	}
+	
+	public int inquiredelete(int inquire_id) { 
+		return sqlSession.delete("customer.inquiredelete", inquire_id); 
+	}//delete() end
+	
 	public int oneInquireins(InquireDTO dto) {
 		return sqlSession.insert("customer.oneinquireins", dto);
 	}
 	public List<InquireDTO> memberinquirelist(String member_id) {
 		return sqlSession.selectList("customer.memberinquirelist", member_id);
 	}
-	
+	//
 	
 }//class end
